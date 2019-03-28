be7d3345edb23de47e2ebb118c14b8ead5c49eef
John Elder <30008225@student.toiohomai.ac.nz>
2019-03-29 12:55:28 +1300

    Queues

---
+++ Q1/Q1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+
+            Console.WriteLine("Please Enter Value: ");      // Chuck in a loop to simplify
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Please Enter Value: ");      //
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Please Enter Value: ");      //
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Please Enter Value: ");      //
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("Please Enter Value: ");      //
+            s1.Push(Console.ReadLine());
+
+            Console.WriteLine("Check for a specific value: ");
+            string input = Console.ReadLine();
+
+            
+            if (s1.Contains(input) == true)
+            {
+                while (s1.Peek().ToString() != input)
+                {
+                    s1.Pop();
+                }
+                
+
+                Console.WriteLine($"Stack contains the value {s1.Peek()}");
+            }
+            else
+            {
+                Console.WriteLine("Does not contain the value");
+                                
+            }
+           
+            Console.ReadLine();     
+        }
+    }
+}
---
+++ QueueQ1/QueueQ1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q1 = new Queue();
+            Queue q2 = new Queue();
+
+
+            string input = "y";
+
+            do
+            {
+                Console.WriteLine("\nEnter name");
+                q1.Enqueue(Console.ReadLine());
+                
+               Console.WriteLine("\nChuck another name in?  (y/n): ");
+               input = Console.ReadLine();
+                              
+            } while (input == "y");
+
+
+
+            do
+            {
+                q2.Enqueue(q1.Peek());
+                Console.WriteLine("\nName " + q1.Dequeue());                     
+
+
+            } while (q1.Count !=0);
+            Console.WriteLine("\nFind a name in the Queue ");
+                        
+            input = Console.ReadLine();
+            if (q2.Contains(input) == true)
+            {
+                while (q2.Peek().ToString() != input)
+                {
+                    q2.Dequeue();
+                }
+
+
+                Console.WriteLine($"\nQueue contains the value {q2.Peek()}");
+            }
+            else
+            {
+                Console.WriteLine("\nDoes not contain the Name" + input);
+            }
+
+            Console.WriteLine(q2.Peek());
+            
+
+            Console.WriteLine("\nAwesome! Have as wicked day");
+            Console.ReadLine();
+        }
+              
+    }
+}
